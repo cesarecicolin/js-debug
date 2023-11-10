ec2df497258dac3c8267d3568fb80ce5b1e273a1
cesarecicolin <cece.caster@gmail.com>
2023-11-10 16:19:30 +0100

    bundle 2

--- js-debug/bundle-02/main.js
+++ js-debug/bundle-02/main.js
@@ -13,32 +13,41 @@
 // ESERCIZIO 1
 function checkAge() {
     const myAge = 32;
-    const message = '';
+    let message = '';
 
     if (myAge < 18) {
         message = `Sei troppo giovane! Hai ${myAge} anni!`;
     } else {
         message = 'Hai più di 18 anni!';
     }
+    return message
 }
+console.log(checkAge());
 checkAge();
-
-// ESERCIZIO 2
-function printColorsNumber() {
-    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
-}
-printColorsNumber();
+// questa funzione controlla l'eta  e ti dice se sei maggiorene o minnorene  
+// ma cerano due problemi  message era una cstante che pero no puo cambiare  e poi questa funzione non ritornava nulla
+//  ESERCIZIO 2
+ function printColorsNumber() {
+     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+     
+     const number = colors.length
+     console.log(`Nella mia palette ci sono ${number} colori!`);
+ }
+ printColorsNumber();
+//  questa funzione di dice quanti colori ci sono nel array 
+//  il numero di colori non era stato inzializato un una variabile
 
 
 // ESERCIZIO 3
-function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
-    const total = userNumber + 12;
+// function addNumbers() {
+//     const userNumber = parseInt(prompt('Inserisci un numero'));
+//     const total = userNumber + 12;
 
-    console.log(`Il risultato finale è ${total}`);
-}
-addNumbers();
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
+// questa funzione chiede un numero per poi stamparlo con un addizione di 12 
+// il problema era che mancava parsint prima del prompt quindi otenevo una stringa invece di un numero
 
 
 // ESERCIZIO 4
@@ -50,44 +59,46 @@ function checkAccess() {
 
     if (addresses.includes(userEmail)) {
         grantAccess = 'true';
+        
     }
-
+console.log(grantAccess);
     if (grantAccess === true) {
         console.log('Accesso consentito!');
     } else {
         console.log('Accesso negato!');
     }
+    console.log(grantAccess);
 }
 checkAccess();
 
 
-// ESERCIZIO 5 (suggerimento: c'è un solo errore)
-function checkAccessImproved() {
-    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+// // ESERCIZIO 5 (suggerimento: c'è un solo errore)
+// function checkAccessImproved() {
+//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    let grantAccess = 'false';
+//     let grantAccess = 'false';
 
-    for (let i = 0; i < addresses.length; i++) {
-        const email = addresses[i];
+//     for (let i = 0; i < addresses.length; i++) {
+//         const email = addresses[i];
 
-        if (userEmail.length > 5) {
+//         if (userEmail.length > 5) {
 
-            if (email === userEmail) {
-                grantAccess = 'true';
+//             if (email === userEmail) {
+//                 grantAccess = 'true';
 
-            }
+//             }
 
-        }
+//         }
 
-        if (grantAccess) {
-            console.log('Accesso consentito!');
-        } else {
-            console.log('Accesso negato!');
-        }
-    }
-    checkAccessImproved();
+//         if (grantAccess) {
+//             console.log('Accesso consentito!');
+//         } else {
+//             console.log('Accesso negato!');
+//         }
+//     }
+//     checkAccessImproved();
 
 
 
